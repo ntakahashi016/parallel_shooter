929500aa00cd79adbfc377e31c08b2fd39982349
ntakahashi016 <45719895+ntakahashi016@users.noreply.github.com>
2021-09-18 10:48:52 +0900

    Update from official hello world

--- src/main.go
+++ src/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-  "log"
-  "github.com/hajimehoshi/ebiten/v2"
-  "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Game struct{}
 
 func (g *Game) Update() error {
-  return nil
+	return nil
 }
 
-func (g *Game) Drow(screen *ebiten.Image) {
-  ebitenutil.DebugPrint(screen, "Hello, World!")
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-  return 320, 240
+	return 320, 240
 }
 
 func main() {
-  ebiten.SetWindowSize(640, 480)
-  ebiten.SetWindowTitle("Hello, World!")
-  if err := ebiten.RunGame(&Game{}); err != nil {
-    log.Fatal(err)
-  }
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
