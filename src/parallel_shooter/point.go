41d8ac6afb64d5c77eb82393d86c4c1e0066193c
ntakahashi016 <ntakahashi016@gmail.com>
2021-12-04 11:08:53 +0900

    fix NewArea

--- src/parallel_shooter/point.go
+++ src/parallel_shooter/point.go
@@ -11,25 +11,3 @@ func NewPoint(x,y int) *Point {
 	p.y = y
 	return p
 }
-
-func (p *Point) direction(other *Point) *Point {
-	x := other.x - p.x
-	switch  {
-	case x > 0:
-		x=1
-	case x < 0:
-		x=-1
-	default:
-		x=0
-	}
-	y := other.y - p.y
-	switch {
-	case y > 0:
-		y=1
-	case y < 0:
-		y=-1
-	default:
-		y=0
-	}
-	return NewPoint(x,y)
-}
